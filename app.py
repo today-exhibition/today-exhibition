d7c18f1b35a6525cb9cbd7b7f075a5dff47b5eb1
ejnow <ejnoh3@gmail.com>
2023-08-14 11:36:55 +0900

    Feat: Artist detail 조회 추가

--- app.py
+++ app.py
@@ -6,15 +6,19 @@ from config import DEBUG, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATION
 
 from models.model import db
 
+from views.artist.artist import artist_bp
+
 
 app = Flask(__name__)
 
 app.instance_path = os.path.join(os.getcwd(), 'database')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
-app.debug = DEBUG
+app.debug = True
 
 db.init_app(app)
+
+app.register_blueprint(artist_bp)
 
 if __name__ == "__main__" :
     with app.app_context():
---
+++ views/artist/artist.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template
+from sqlalchemy import func
+from datetime import datetime
+from models.model import db, Artist, Exhibition, Gallery, ArtistExhibition
+
+artist_bp = Blueprint('artist', __name__)
+
+#! [작가디테일 > 작가 정보 조회(작가명, 작가이미지)] #작가 이미지 삭제(8/11)
+@artist_bp.route('/artist/<id>')
+def artist(id):
+    artist = db.session.query(
+            Artist.name)\
+                .filter(Artist.id == id).first()
+
+#! [작가디테일 > 전시 정보 조회(전시명, 미술관명, 전시기간, 전시이미지)]
+    exhibitions = db.session.query(
+                Exhibition.title, 
+                Gallery.name, 
+                Exhibition.start_date, 
+                Exhibition.end_date, 
+                Exhibition.thumbnail_img)\
+                .join(Gallery, Exhibition.gallery_id == Gallery.id)\
+                .join(ArtistExhibition, ArtistExhibition.exhibition_id == Exhibition.id)\
+                .join(Artist, Artist.id == ArtistExhibition.artist_id)\
+                .filter(Artist.id == id).all()
+    
+#! [작가디테일 > 진행/예정/종료 전시 상태 (오늘 날짜와 비교)]
+    today = datetime.today().date()  #오늘 날짜
+    ongoing_exhibitions = []       #진행중 전시 
+    upcoming_exhibitions = []      #예정중 전시
+    ended_exhibitions = []         #지난 전시
+
+    for exhibition in exhibitions:
+        if exhibition[2] <= today and exhibition[3] >= today:
+            ongoing_exhibitions.append(exhibition)
+        elif exhibition[2] > today:
+            upcoming_exhibitions.append(exhibition)
+        elif exhibition[3] < today:
+            ended_exhibitions.append(exhibition)
+
+
+    return render_template('artist.html', artist=artist, 
+                            ongoing_exhibitions=ongoing_exhibitions,
+                            ongoing_count=len(ongoing_exhibitions),
+                            upcoming_exhibitions=upcoming_exhibitions,
+                            upcoming_count=len(upcoming_exhibitions),                    
+                            ended_exhibitions=ended_exhibitions,
+                            ended_count=len(ended_exhibitions), id=id)
+                            
