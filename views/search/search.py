67fe5233ed2d43b3ae933985b0c412003cb555ed
etesong014 <etesong0114@gmail.com>
2023-08-14 11:39:36 +0900

    Feat: /search/artist 작가 검색 결과 - 간단한 페이지 작업

--- views/search/search.py
+++ views/search/search.py
@@ -52,3 +52,32 @@ def search_exhibition():
     exhibition_count = len(exhibitions)
     
     return render_template('search/search_exhibition.html', exhibitions=exhibitions, keyword=keyword, exhibition_count=exhibition_count)
+
+@search_bp.route('/search/artist')
+def search_artist():
+    keyword = request.args.get('keyword', default="", type=str).strip()
+
+    exhibitions = Exhibition.query \
+                    .with_entities(
+                    Exhibition.id,
+                    Exhibition.title,
+                    Exhibition.start_date,
+                    Exhibition.end_date,
+                    Exhibition.gallery_id,
+                    Exhibition.thumbnail_img
+                    ) \
+                    .filter(Exhibition.title.like('%' + keyword + '%')) \
+                    .order_by(Exhibition.start_date) \
+                    .all()
+    exhibition_count = len(exhibitions)
+    # artists = Artist.query \
+    #             .with_entities(
+    #             Artist.id,
+    #             Artist.name,
+    #             Artist.thumbnail_img
+    #             ) \
+    #             .filter(Artist.name.like('%' + keyword + '%')) \
+    #             .order_by(Artist.id) \
+    #             .limit(9)
+
+    return render_template('search/search_artist.html', exhibitions=exhibitions, keyword=keyword, exhibition_count=exhibition_count)
