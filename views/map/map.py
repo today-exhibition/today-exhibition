8a62a2b7ec9c96054b754304cd08b35335037d18
tori <sangeun99@khu.ac.kr>
2023-08-18 14:28:55 +0900

    Feat: 지도 인기순 조회 기능 추가

--- views/map/map.py
+++ views/map/map.py
@@ -1,8 +1,9 @@
 import json
 import datetime
 from flask import Blueprint, render_template
+from sqlalchemy import func
 
-from models.model import db, Exhibition, Gallery, GalleryAddress
+from models.model import db, Exhibition, Gallery, GalleryAddress, LikeExhibition
 
 map_bp = Blueprint('map', __name__)
 
@@ -68,7 +69,32 @@ def featured():
 #! [인기순 : 전체 하트순 전시]
 @map_bp.route('/map/popular')
 def popular():
-    pass
+    with open("secrets.json", "r") as secrets_file:
+        secrets = json.load(secrets_file)
+    kakao_map_api_key = secrets["map"]["kakao_api_key"]
+
+    map_list = db.session.query(func.count('*'), Exhibition.id.label("exhibition_id"), Exhibition.title.label("exhibition_title"), Exhibition.thumbnail_img, Exhibition.start_date, Exhibition.end_date, Gallery.name.label("gallery_name"), GalleryAddress.gpsx, GalleryAddress.gpsy) \
+      .join(Gallery, Gallery.id == Exhibition.gallery_id) \
+      .join(GalleryAddress, Gallery.id == GalleryAddress.gallery_id) \
+      .join(LikeExhibition, LikeExhibition.exhibition_id == Exhibition.id) \
+      .group_by(LikeExhibition.exhibition_id) \
+      .order_by(func.count('*').desc()) \
+      .all()
+    
+    result_list = []
+    for item in map_list:
+        result_list.append({
+            "exhibition_id": item.exhibition_id,
+            "exhibition_title": item.exhibition_title,
+            "thumbnail_img": item.thumbnail_img,
+            "start_date": str(item.start_date),
+            "end_date": str(item.end_date),
+            "gallery_name": item.gallery_name,
+            "gpsx": item.gpsx,
+            "gpsy": item.gpsy
+        })
+    exhibition_list = json.dumps(result_list, ensure_ascii=False)
+    return render_template("map/map.html", api_key=kakao_map_api_key, exhibition_list=exhibition_list, type="popular")
 
 #! [추천 전시 : 큐레이팅 전시]
 @map_bp.route('/map/recommended')
