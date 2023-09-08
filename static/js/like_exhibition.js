73f986f14fb30e8e2036655c1dd24ddda4e84b5a
etesong014 <etesong0114@gmail.com>
2023-09-08 21:35:15 +0900

    Fix: 좋아요, 팔로우 기능 서버에서 세션 처리 - 세션 없는 경우 알럿창 띄우기 #20

--- static/js/like_exhibition.js
+++ static/js/like_exhibition.js
@@ -9,7 +9,9 @@ function likeExhibition(e, icon, exhibition_id) {
     url: url,
     data: { exhibition_id: exhibition_id },
     success: function (resp) {
-      if (resp == "liked") {
+      if (resp == "login_required") {
+        window.alert("로그인 후 이용 가능한 서비스입니다.")
+      } else if (resp == "liked") {
         icon.classList.remove("off");
         icon.classList.add("on");
       } else if (resp == "unliked") {
