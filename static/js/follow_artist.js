16952d915a6c1a1c7c59167c9d37d34c30fe9dfe
etesong014 <etesong0114@gmail.com>
2023-09-11 14:04:26 +0900

    Feat: 기존 알럿창을 모달창으로 변경 #20

--- static/js/follow_artist.js
+++ static/js/follow_artist.js
@@ -8,7 +8,7 @@ function followArtist(icon, artist_id) {
     data: { artist_id: artist_id }, 
     success: function (resp) {
       if (resp == "login_required") {
-        window.alert("로그인 후 이용 가능한 서비스입니다.")
+        $('#alertmodal').modal('show');
       } else if (resp == "followed") {
         icon.classList.remove("off");
         icon.classList.add("on");
