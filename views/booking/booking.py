71b41b5efc6d4e5c959ae87f62cb81d1f0b03c47
tori <sangeun99@khu.ac.kr>
2023-09-11 11:57:47 +0900

    Remove: 부킹 탭 삭제 #11

--- views/booking/booking.py
+++ views/booking/booking.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, session, request
-from flask import redirect, url_for
 from datetime import datetime, timedelta
 from sqlalchemy import and_
 from utils import load_secrets
@@ -14,36 +13,6 @@ from decorators import check_user_login
 
 
 booking_bp = Blueprint('booking', __name__)
-
-@booking_bp.route('/booking')
-@check_user_login
-def booking():
-    page = request.args.get('page', default=1, type=int)
-    current_date = datetime.now().date()
-    
-    user_id = session.get('user_id', None)
-    available_exhibitions = db.session.query(
-                                Exhibition.id,
-                                Exhibition.title,
-                                Exhibition.start_date,
-                                Exhibition.end_date,
-                                Gallery.name,
-                                Exhibition.thumbnail_img
-                            ) \
-                            .filter(and_(Exhibition.start_date <= current_date, Exhibition.end_date >= current_date)) \
-                            .join(Gallery, Exhibition.gallery_id == Gallery.id) \
-                            .all()
-    
-    exhibition_count = len(available_exhibitions)
-    total_pages, current_page, page_data, page_list = calc_pages(available_exhibitions, page)
-    
-    liked_exhibition_ids = []  
-    if user_id:
-        liked_exhibition_ids = [like.exhibition_id for like in LikeExhibition.query.filter_by(user_id=user_id).all()]
-
-    return render_template('search/search_exhibition.html', 
-                           exhibitions=page_data, exhibition_count=exhibition_count, liked_exhibition_ids=liked_exhibition_ids, 
-                           total_pages=total_pages, current_page=current_page, page_list=page_list)
 
 #! TODO: ticket_price 없으면 0원으로 진행
 #! TODO: 나중에 데이터 추가 후 예외처리 추가하기
